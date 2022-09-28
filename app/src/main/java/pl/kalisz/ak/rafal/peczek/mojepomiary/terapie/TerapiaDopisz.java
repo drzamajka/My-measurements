c5bcf9b8256e63ddbf910b8c86d89ec8187a7b3e
drzamajka <drzamajka999@gmail.com>
2022-09-28 13:33:17 +0200

    rozbudowa terapi o dynamiczne dodawanie godzin pomiarów

--- app/src/main/java/pl/kalisz/ak/rafal/peczek/mojepomiary/terapie/TerapiaDopisz.java
+++ app/src/main/java/pl/kalisz/ak/rafal/peczek/mojepomiary/terapie/TerapiaDopisz.java
@@ -2,16 +2,23 @@ package pl.kalisz.ak.rafal.peczek.mojepomiary.terapie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import pl.kalisz.ak.rafal.peczek.mojepomiary.R;
@@ -20,46 +27,145 @@ import pl.kalisz.ak.rafal.peczek.mojepomiary.repository.UsersRoomDatabase;
 
 public class TerapiaDopisz extends AppCompatActivity {
 
-    private int liczbaElementow;
-    private ArrayList<View> listaElementowl;
-    private Spinner pomiary;
-    private DatePicker dataRozpoczecia, dataZakonczenia;
-    private LinearLayout listaElementow;
+    private int czestotliwoscView;
+    private ArrayList<View> listaElementowL, listaCzestotliwosciL;
+    private Spinner czestotliwosc;
+    private EditText dataRozpoczecia, dataZakonczenia;
+    private LinearLayout listaElementow, listaCzestotliwosci;
     private UsersRoomDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terapia_dopisz);
-        liczbaElementow = 0;
-        listaElementowl = new ArrayList<>();
-        dataRozpoczecia = (DatePicker) findViewById(R.id.dataRozpoczecia);
-        dataZakonczenia = (DatePicker) findViewById(R.id.dataZakonczenia);
+        listaElementowL = new ArrayList<>();
+        listaCzestotliwosciL = new ArrayList<>();
+        dataRozpoczecia = (EditText) findViewById(R.id.dataRozpoczecia);
+        dataZakonczenia = (EditText) findViewById(R.id.dataZakonczenia);
         listaElementow = (LinearLayout) findViewById(R.id.listaElementow);
-        //pomiary = (Spinner) findViewById(R.id.spinnerPomiary);
-
+        listaCzestotliwosci = (LinearLayout) findViewById(R.id.listaCzestotliwosci);
+        czestotliwosc = (Spinner) findViewById(R.id.czestotliwosc);
         database = UsersRoomDatabase.getInstance(getApplicationContext());
-        List<Pomiar> listaPomiarow = database.localPomiarDao().getAll();
 
-        //jednostka.
-        int i = 0;
-        String[] strPomiarow = new String[listaPomiarow.size()];
-        for (Pomiar pomiar: listaPomiarow) {
-            strPomiarow[i] = pomiar.getNazwa();
-            i++;
+        this.dodajElement(new View(getApplicationContext()));
+        this.dodajDatePicker(dataRozpoczecia);
+        this.dodajDatePicker(dataZakonczenia);
+
+        czestotliwosc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "wybrano id:"+i+l, Toast.LENGTH_LONG).show();
+                View elementView = getczestotliwoscView(true);
+                listaCzestotliwosci.removeAllViews();
+                listaCzestotliwosciL.add(elementView);
+                listaCzestotliwosci.addView(elementView);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getApplicationContext(), "nic nnie wybrano", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private View getczestotliwoscView(Boolean isOnItemChange){
+        View elementView;
+        switch ((int) czestotliwosc.getSelectedItemId()) {
+            case 1:
+                elementView = getLayoutInflater().inflate(R.layout.activity_terapia_dopisz_x_godzin, null, false);
+                break;
+            case 2:
+                elementView = getLayoutInflater().inflate(R.layout.activity_terapia_dopisz_x_dni, null, false);
+                break;
+            default:
+                elementView = getLayoutInflater().inflate(R.layout.activity_terapia_dopisz_x_razy, null, false);
+                EditText editText = elementView.findViewById(R.id.editTextTime);
+                Button button = (Button) elementView.findViewById(R.id.usunGodzine);
+                button.setEnabled(false);
+                dodajTimePicker(editText);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(listaCzestotliwosciL.size()>2) {
+                            listaCzestotliwosci.removeView(elementView);
+                            listaCzestotliwosciL.remove(elementView);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Terapia musi posiadać conajmniej jedną godzine", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+                break;
         }
+        return elementView;
+    }
 
-        ArrayAdapter<String > adapter = new ArrayAdapter<String> (getApplicationContext(),
-                android.R.layout.simple_list_item_1, strPomiarow);
+    private void dodajTimePicker(EditText editText){
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
 
-        //pomiary.setAdapter(adapter);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        TerapiaDopisz.this,
+                        new TimePickerDialog.OnTimeSetListener(){
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                if(editText.getText().toString().length() <= 0) {
+                                    View elementView = getczestotliwoscView(false);
+                                    listaCzestotliwosciL.get(listaCzestotliwosciL.size()-1).findViewById(R.id.usunGodzine).setEnabled(true);
+                                    listaCzestotliwosciL.add(elementView);
+                                    listaCzestotliwosci.addView(elementView);
+                                }
+                                if(minute>9)
+                                    editText.setText(hourOfDay + ":" + minute);
+                                else
+                                    editText.setText(hourOfDay + ":0" + minute);
 
+                            }
+                        },
+                        hour, minute, DateFormat.is24HourFormat(TerapiaDopisz.super.getApplicationContext()));
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    private void dodajDatePicker(EditText editText){
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        TerapiaDopisz.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                editText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 
     public void dodajElement(View view) {
         View elementView = getLayoutInflater().inflate(R.layout.activity_terapia_dopisz_element, null, false);
         //elementView.setId(liczbaElementow++);
-        listaElementowl.add(elementView);
+        listaElementowL.add(elementView);
         LinearLayout listaElementow = (LinearLayout) findViewById(R.id.listaElementow);
 
         Spinner spinner = (Spinner) elementView.findViewById(R.id.spinnerPomiary);
@@ -79,8 +185,13 @@ public class TerapiaDopisz extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                listaElementow.removeView(elementView);
-                listaElementowl.remove(elementView);
+                if(listaElementowL.size()>1) {
+                    listaElementow.removeView(elementView);
+                    listaElementowL.remove(elementView);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Terapia musi posiadać conajmniej jedną składową", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
@@ -91,7 +202,7 @@ public class TerapiaDopisz extends AppCompatActivity {
 
         //LinearLayout listaElementow = (LinearLayout) findViewById(R.id.listaElementow);
         ArrayList listaWybranych = new ArrayList();
-        for(View v: listaElementowl){
+        for(View v: listaElementowL){
             Spinner spinner = (Spinner) v.findViewById(R.id.spinnerPomiary);
             listaWybranych.add(spinner.getSelectedItem());
         }
